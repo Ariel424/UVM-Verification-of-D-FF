2423854af2f428d5551369806882088eea5a9be6
Ariel Topaz <130974714+Ariel424@users.noreply.github.com>
2024-04-12 13:57:41 +0300

    Create Design Code.sv

---
+++ Design Code.sv
@@ -0,0 +1,26 @@
+module Dff (
+  input clk;
+  input rst; 
+  input din; 
+  output reg dout;
+); 
+
+  always@(posedge clk)
+    begin
+      if (rst)
+        dout <= 1'b0;
+      else
+        dout <= din; 
+    end 
+endmodule 
+
+interface dff_if; 
+  logic clk; 
+  logic rst; 
+  logic din;
+  logic dout; 
+endinterface 
+
+
+
+  
