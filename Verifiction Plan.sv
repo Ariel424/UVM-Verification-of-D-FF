a7bac8874c212e2f0ab7f31fc81a195b78cf8641
Ariel Topaz <130974714+Ariel424@users.noreply.github.com>
2024-04-15 16:38:22 +0300

    Update Verifiction Plan.sv

--- Verifiction Plan.sv
+++ Verifiction Plan.sv
@@ -176,52 +176,117 @@ class drv extends uvm_driver #(transaction);
                           $display ("-----------------------------------------");
                   endfunction 
                 endclass 
-                
 
-                        
+                class agent extends uvm_agent; 
+                  `uvm_component_utils (agent)
 
-                          
-                        
+                  function new (input string inst = "agent", uvn_conponent parent = null); 
+                    super.new(inst, parent); 
+                  endfunction 
 
-                  
-              
+                  drv v; 
+                  uvm_sequencer#(transaction) seqr;
+                  mon m;
 
-        
-          
+                  config_dff cfg;
 
-                
-                
+                  virtual function void build_phase (uvm_phase phase);
+                  super.build_phase (phase);
 
+                    m = mon::type_id::create ("m", this);
+                    cfg = config_dff::type_id::create ("cfg"); 
 
-        
-                 
-          
-                 
-            
-            
-          
-          
-          
-      
-          
-        
-        
-        
+                    if (!uvm_config_db#(config_dff)::get(this, " ", "cfg", cfg)
+                        `uvm_error ("Agent", "Failed to access confif"); 
+                        if (cfg.agent_type == UVM_ACTIVE)
+                          begin 
+                            d = drv::type_id::create("d", this); 
+                            seqr = uvm_sequencer#(transaction)::type_id::create("seqr", this);
+                          end 
+                        endfunction 
 
-    
-          
-            
-        
-        
-    
-    
-    
+                        virtual function void connect_phase (uvm_phase phase);
+                          super.connect_phase (phase); 
+                          d.seq_item_port.connect(seqr.seq_item_export);
+                        endfunction
+                        endclass 
 
-  
-  
+                        class env extends uvm_env; 
+                          `uvm_component_utils (env)
 
+                          function new (input string inst = "env", uvm_component c); 
+                            super.new (inst, c); 
+                          endfunction
 
-  
+                          agent a; 
+                          sco s; 
+                          config_dff cfg; 
 
-  
-          
+                          virtual function void build_phase (uvm_phase phase);
+                            super.build_phase(phase);
+                            a = agent::type_id::create("a", this);
+                            s = sco::type_id::create("s", this); 
+                            cfg = config_dff::type_id::create("cfg"); 
+                            uvm_config_db#(config_dff)::set(this, "a", "cfg", cfg);
+                          endfunction
+
+                          virtual function void connect_phase(uvm_phase phase); 
+                            super.connect_phase(phase);
+                            a.m.send.connect(s.recv);
+                          endfunction 
+                        endclass 
+
+                        class test extends uvm_test;
+                          `uvm_componemt_utils (test)
+
+                          function new (input string inst = "test", uvm_component c);
+                            super.new(inst, c);
+                          endfunction 
+
+                          env e;
+                          vaild_din vdin; 
+                          rst_dff rff; 
+                          rand_din_rst rdin; 
+
+                          virtual function void build_phase (uvm_phase phase);
+                            super.build_phase (phase); 
+                            e = env::type_id::create ("env", this); 
+                            vdin = vaild_din::type_id::create ("vdin");
+                            rff = rst_dff::type_id::create ("rff");
+                            rdin = rand_din_rst::type_id::create ("rdin");
+                          endfunction
+
+                          virtual task run_phase (uvm_phase phase);
+                            phase.raise_objection (this); 
+                            rff.start (e.a.seqr);
+                            #40; 
+                            vdin.start(e.a.seqr);
+                            #40; 
+                            rdin.start(e.a.seqr);
+                            #40
+                            phase.drop_onjection(this);
+                          endtask 
+                        endclass 
+
+                        module tb; 
+                        dff_if dif();
+
+                        dff dut (.clk(dif.clk), .rst(dif.rst), .din(dif.din), .dout(dif, dout));
+
+                        initial 
+                          begin 
+                            uvm_config_db#(virtual dff_if)::set(null, "*", "dif", dif);
+                            run_test ("test");
+                          end 
+
+                          initial begin
+                            dif.clk = 0;
+                          end 
+
+                          always #10 dif.clk = ~dif.clk; 
+
+                          initial begin 
+                            $dumpfile ("dump.vcd");
+                            $dumpvars;
+                          end 
+                        endmodule 
