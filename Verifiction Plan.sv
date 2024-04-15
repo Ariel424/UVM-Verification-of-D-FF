88ad62dc5f77f02780f27dcbeec19457eaa93ccb
Ariel Topaz <130974714+Ariel424@users.noreply.github.com>
2024-04-15 12:34:36 +0300

    Update Verifiction Plan.sv

--- Verifiction Plan.sv
+++ Verifiction Plan.sv
@@ -59,11 +59,169 @@ class rst_dff extends uvm_sequence#(transaction);
   virtual task body ();
     repeat (15)
       begin 
-        tr = transaction::type_id::create ("tr); 
-        start_item(tr);
+        tr = transaction::type_id::create("tr"); 
+        start_item (tr);
         assert (tr.randomize()); 
-        tr.rst = 1'b1;
-          `uvm_info("SEQ", $sformatf("rst : %0b  din : %0b  dout : %0b", tr.rst, tr.din, tr.dout), UVM_NONE);
-          finish_item(tr)
-                                           
-                                           
+        tr.rst = 1'b1; 
+        `uvm_info ("SEQ", $sformatf ("rst : %0b  din : %0b  dout : %0b", tr.rst, tr.din, tr.dout), UVM_NONE);
+        finish_item (tr); 
+      end 
+  endtask
+endclass 
+
+class rand_bit_rst extends uvm_sequence#(transaction); 
+  `uvm_object_utils (rand_bit_rst)
+
+  transaction tr; 
+
+  function new (input string path = "rand_bit_rst"); 
+    super.new (path); 
+  endfunction 
+
+  virtual task body (); 
+    repeat (15) 
+      begin 
+        tr = transaction::type_id::create ("tr"); 
+        start_item (tr); 
+        assert (tr.randomize()); 
+        `uvm_info ("SEQ", $sformatf ("rst : %0b  din : %0b  dout : %0b", tr.rst, tr.din, tr.dout), UVM_NONE);
+        finish_item(tr);
+      end
+  endtask 
+endclass 
+
+class drv extends uvm_driver #(transaction); 
+  `uvm_component_utils (drv)
+
+  transaction tr; 
+  virtual dff_if dif; 
+
+  function new (input string path = "drv", uvm_component parent = null); 
+    super.new(path, parent)
+  endfunction 
+
+  virtual function void build_phase (uvm_phase phase); 
+    super.build_phase (phase);
+    if (!uvm_config_db#(virtual dff_if)::get(this, " ", "dif", dif)
+        `uvm_error ("drv", "Unable to access Interface"); 
+        endfunction
+
+        virtual task run_phase (uvm_phases phase); 
+          tr = transaction::type_id::create("tr"); 
+          forever begin 
+            seq_item_port.get_next_item(tr); 
+            dif.rst <= tr.rst; 
+            dif.din <= tr.din;
+            `uvm_info ("SEQ", $sformatf ("rst : %0b  din : %0b  dout : %0b", tr.rst, tr.din, tr.dout), UVM_NONE);
+            seq_item_port.item_done(); 
+            repeat (2) @(posedge dif.clk); 
+          end
+        endtask
+        endclass 
+
+        class mon extends uvm_monitor; 
+          `uvm_component_utils (mon)
+
+          uvm_analysis_port#(transaction) send; 
+          transaction tr; 
+          virtual dff_if dif; 
+
+          function new (input string inst = "mon", uvm_component parent = null);
+            super.new (inst, null);
+          endfunction
+
+          virtual function void build_phase (uvm_phase phase);
+            super.build_phase(phase);
+            tr = transaction::type_id::create("tr");
+            send = new("send", this);
+            if (!uvm_config_db#(virtual dff_if)::get(this, " ", "dif", dif)
+                `uvm_error ("drv", "Unable to access Interface"); 
+                endfunction 
+
+                virtual task run_phase (uvm_phase phase); 
+                  forever begin 
+                    repeat (2) @(posedge dif.clk); 
+                    tr.rst = dif.rst;
+                    tr.din = dif.din; 
+                    tr.dout = dif.dout; 
+                    `uvm_info ("MON", $sformatf ("rst : %0b  din : %0b  dout : %0b", tr.rst, tr.din, tr.dout), UVM_NONE);
+                    send.write(tr);
+                  end 
+                endtask 
+                endclass 
+
+                class sco extends uvm_scoreboard 
+                  `uvm_component_utils (sco)
+
+                  uvm_analysis_imp# (transaction, sco) recv; 
+
+                  function new (input string inst = "sco", uvm_component parent = null); 
+                    super.new (inst, parent);
+                  endfunction 
+
+                  virtual function void build_phase (uvm_phase phase); 
+                    super.new (phase); 
+                    recv = new ("recv", this);
+                  endfunction 
+
+                  virtual function void write (transaction tr);
+                    `uvm_info ("SCO", $sformatf("rst : %0b  din : %0b  dout : %0b", tr.rst, tr.din, tr.dout), UVM_NONE);
+                    if (tr.rst == 1'b1)
+                      `uvm_info ("SCO", "DFF Reset", UVM_NONE)
+                      else if (tr.rst == 1'b0 && (tr.din == tr.dout))
+                        `uvm_info ("SCO", "TEST PASSED", UVM_NONE)
+                        else 
+                          `uvm_info ("SCO", "TEST FAILED", UVM_NONE)
+
+                          $display ("-----------------------------------------");
+                  endfunction 
+                endclass 
+                
+
+                        
+
+                          
+                        
+
+                  
+              
+
+        
+          
+
+                
+                
+
+
+        
+                 
+          
+                 
+            
+            
+          
+          
+          
+      
+          
+        
+        
+        
+
+    
+          
+            
+        
+        
+    
+    
+    
+
+  
+  
+
+
+  
+
+  
+          
